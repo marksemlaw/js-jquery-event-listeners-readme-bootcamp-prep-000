5b51a1bae8e055860cd3a020f24d1a6f24a012d3
Mark Semotiuk <marksem@gmail.com>
2020-04-27 20:15:06 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -18,7 +18,7 @@ function pressIt(){
 
 function submitIt(){
   $('form').on("submit", function(){
-  return alert("Hey!")})
+  return alert("Your form is going to be submitted now.")})
 }
 
 $(document).ready(function(){
