3f67c89cc9d4472e272cd637bbe850c9aefd9fa2
Mark Semotiuk <marksem@gmail.com>
2020-04-27 19:43:07 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,22 @@
-//define functions here
+function getIt(){
+  $('p').on("click", alert("Hey!"))
+}
+
+function frameIt(){
+  $('p').on("click", alert("Hey!"))
+}
+
+function pressIt(){
+  $('p').on("click", alert("Hey!"))
+}
+
+function submitIt(){
+  $('p').on("click", alert("Hey!"))
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
 });
